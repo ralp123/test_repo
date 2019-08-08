97f6b4178374ccc9f5cfea14bc03187e6a306704
Ralp T Mancawan <rmancawan@rmancawan.innovuze>
2019-08-08 13:57:14 +0800

    Initial Commit

---
+++ src/Square.js
@@ -0,0 +1,24 @@
+import React from 'react'
+//import ReactDOM from 'react-dom'
+
+// class Square extends React.Component {
+//     render() {
+// 		return (
+// 			<button className="square" onClick={(props.onClick}>
+// 				{this.props.value}
+// 			</button>
+// 		);
+//     }
+// }
+function Square(props) {
+	return (
+		<button className="square" onClick={props.onClick}>
+			{props.value}
+		</button>
+	);
+}
+
+export default Square
+  
+
+  
